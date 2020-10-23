d2988e8c7fa367bd3f391be51755ba27935c1d40
wyc2019100 <1042718192@qq.com>
2020-10-23 11:52:38 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ics_lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ics_lab1.dir/main.cpp.o"
+  "ics_lab1.pdb"
+  "ics_lab1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ics_lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ics_lab1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ics_lab1.
+# This may be replaced when dependencies are built.
